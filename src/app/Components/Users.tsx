2ab8e7543c4b568d51ad25aeb1331db24c3d5821
velo1203 <hokitoki1203@gmail.com>
2024-07-22 17:36:23 +0900

    add

--- src/app/Components/Users.tsx
+++ src/app/Components/Users.tsx
@@ -1,37 +1,38 @@
 import { useEffect, useState } from "react";
 import styles from "./Users.module.css";
 import axios from "axios";
+import Image from "next/image";
 import { toast } from "react-toastify";
-
 export default function Users() {
     const [users, setUsers] = useState([]);
 
-    useEffect(() => {
+    const fetchData = () => {
         axios.get("/api/points").then((res) => {
             setUsers(res.data);
+            toast.success("갱신했습니다!");
         });
+    };
+
+    useEffect(() => {
+        fetchData(); // 초기 데이터 로드
+
+        const interval = setInterval(() => {
+            fetchData();
+        }, 20000); //5초마다 갱신
+        return () => clearInterval(interval); // 컴포넌트 언마운트 시 타이머 정리
     }, []);
 
     return (
         <div className={styles.tableContainer}>
             <div className={styles.title}>
                 <h1>랭킹</h1>
-                <button
-                    onClick={() => {
-                        axios.get("/api/points").then((res) => {
-                            setUsers(res.data);
-                        });
-                        toast.success("갱신했습니다!");
-                    }}
-                >
-                    새로고침
-                </button>
+                <button onClick={fetchData}>새로고침</button>
             </div>
             {users.length > 0 && (
                 <table className={styles.table}>
                     <thead>
                         <tr>
-                            <th>순서</th>
+                            <th>이미지</th>
                             <th>이름</th>
                             <th>개수</th>
                         </tr>
@@ -39,7 +40,14 @@ export default function Users() {
                     <tbody>
                         {users.map((user: any, i) => (
                             <tr key={user.id}>
-                                <td>{i + 1}</td>
+                                <td>
+                                    <Image
+                                        src={user.picture}
+                                        alt=""
+                                        width={40}
+                                        height={40}
+                                    />
+                                </td>
                                 <td>{user.name}</td>
                                 <td>{user.points}</td>
                             </tr>
